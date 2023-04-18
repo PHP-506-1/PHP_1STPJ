0bfa44c11bccad0e65b2797c1d3e93564a926805
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-18 20:39:19 +0900

    get_db_conn()

--- personal/수빈님/db_conn.php
+++ personal/수빈님/db_conn.php
@@ -1,28 +1,28 @@
 
 <?php
 
-// PDO 인스턴스 생성
-function db_connect()
+function get_db_conn()
 {
     $host = "localhost";
     $user = "root";
     $pass = "root506";
     $charset = "utf8mb4";
     $db_name = "morning_project";
-    $dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
-
-    $pdo_options = [
-        PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ];
-
-    try {
-        return new PDO($dsn, $user, $pass, $pdo_options);
-    } catch (PDOException $e) {
-        exit("데이터베이스 연결에 실패하였습니다.");
+    $dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
+    $pdo_option =
+        array(
+            PDO::ATTR_EMULATE_PREPARES        => false,
+            PDO::ATTR_ERRMODE                => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE     => PDO::FETCH_ASSOC
+        );
+    
+    try
+    {
+        $db_conn = new PDO($dns, $user, $pass, $pdo_option);
+        return $db_conn;
+    }
+    catch(PDOException $e)
+    {
+        throw new Exception($e->getMessage());
     }
 }
-
-// 데이터베이스 연결
-$db = db_connect();

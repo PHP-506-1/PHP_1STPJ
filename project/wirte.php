35b5f14c739132d41d5ca3302f7f1dd5f1259fc7
sjklan4 <126547805+sjklan4@users.noreply.github.com>
2023-04-19 17:40:03 +0900

    0419wirte_insert

--- project/wirte.php
+++ project/wirte.php
@@ -4,26 +4,38 @@ define( "URL_DB", DOC_ROOT."project/DB/db_conn.php");
 include_once( URL_DB );
 
 $http_method = $_SERVER["REQUEST_METHOD"];
-
-//레코드 전체 수를 가져오는 함수 - 작성된 글을 넣기 위해 db의 레코드 순번을 가져오기 위한 코드
-function task_recode_cnt()
+if ( $http_method === "POST" )
 {
-    $sql = " SELECT " 
-    ." count(*) " 
-    ." FROM " 
-    ." task "
-    ;
-
-    $arr_prepare = array();
-
-    $conn = null;
-    $db_conn($conn);
-    $stmt = $obj_conn->prepare($sql); // Prepare Statement를 생성
-    $stmt->execute($arr_prepare); //쿼리 실행
-    $result = $stmt->fetchAll();
+    $arr_post = $_POST;
+    
+    $result_cnt = write_info( $arr_post );
+    var_dump($result_cnt);
+    exit();
 }
 
-return $result;
+//레코드 전체 수를 가져오는 함수 - 작성된 글을 넣기 위해 db의 레코드 순번을 가져오기 위한 코드
+// function task_recode_cnt()
+// {
+//     $sql = " SELECT " 
+//     ." count(*) " 
+//     ." FROM " 
+//     ." task "
+//     ;
+
+//     $arr_prepare = array();
+
+//     $conn = null;
+
+    
+//     get_db_conn($conn);
+//     $stmt = $conn->prepare($sql); // Prepare Statement를 생성
+//     $stmt->execute($arr_prepare); //쿼리 실행
+//     $result = $stmt->fetchAll();
+
+//     return $result;
+// }
+
+
 
 //DB에 입력될 데이터 레코드를 입력하는 sql문
 function write_info(&$param_arr)
@@ -33,51 +45,57 @@ function write_info(&$param_arr)
         ." ,start_time "
         ." ,end_time "
         ." ,task_title "
-        ." ,is_com "
+        // ." ,is_com "
         ." ,task_memo "
         ." ,category_no "
         ." ) "
 
         ." VALUES ( " 
-        ." DATE(NOW()) "
+        ." :task_date "
         ." ,:start_time "
         ." ,:end_time "
         ." ,:task_title "
-        ." ,:is_com "
+        // ." ,:is_com "
         ." ,:task_memo "
-        // ." ,:category_no "
+        ." ,:category_no "
         ." ) "
         ;
 // prepare로 데이터들의 배열을 입력
         $arr_prepare = 
         array(
-            ":start_time" => $param_arr["start_time"]
-            ,":end_time" => $parma_arr["end_time"]
-            ,":task_title" => $parma_arr["task_title"]
-            ,":is_com" => $parma_arr["is_com"]
-            ,":task_memo" => $parma_arr["task_memo"]
-            // ,":category_no" => $parma_arr["category_no"]
+            ":task_date" => $param_arr["task_date"]
+            ,":start_time" => $param_arr["start_time"]
+            ,":end_time" => $param_arr["end_time"]
+            ,":task_title" => $param_arr["task_title"]
+            // ,":is_com" => $param_arr["is_com"]
+            ,":task_memo" => $param_arr["task_memo"]
+            ,":category_no" => $param_arr["category_no"]
         );
 
 
-        $conn = null;
-
-$db_conn($conn);
-$stmt = $obj_conn->prepare($sql); // Prepare Statement를 생성
-$stmt->execute($arr_prepare); //쿼리 실행
-$result = $stmt->fetchAll();
-
+        $db_conn = null;
+        try 
+        {
+            $db_conn= get_db_conn(); //PDO object 셋
+            $db_conn->beginTransaction(); //Transaction 시작 : 데이터를 변경하기(insert, update, delete) 때문에 일련의 연산이 완료되면 commit 실패시 rollback을 통해서 데이터를 관리 하게 시킨다. 
+            $stmt = $db_conn->prepare( $sql ); //statement object 셋팅
+            $stmt->execute( $arr_prepare ); //DB request
+            $result_cnt = $stmt->rowCount(); // 업데이트 되서 영향을 받은 행의 숫자를 가져온다.
+            $db_conn->commit();
+        } 
+        catch ( Exception $e) 
+        {
+            $db_conn->rollback(); // 트랜잭션이 진행중에 오류가 나면 롤백을 시켜서 돌려 준다.
+            return $e->getMessage();
+        }
+        finally //성공여부와 상관없이 null로 커넥션을 초기화 시켜준다.
+        {
+            $db_conn = null;
+        }
 }
 
 
-if ( $http_method === "POST" )
-{
-    $arr_post = $_POST;
-    
-    $result_cnt = write_info( $arr_post );
-    
-    exit();
-}
+
 
 ?>
 
@@ -91,44 +109,46 @@ if ( $http_method === "POST" )
 </head>
 <body>
     <form method = "post" action = "">
-    <div class = date_title><label id = "date_title">
-        <input type="date" id = "date_title" name = 'task_date'></label>
-    </div>
-    <br>
-    <label for ="start_time">시작시간<input type="time" id = "start_time" name = "start_time"></label>
-    <br>
-    <label for ="end_time">종료시간<input type="time" id= "end_time" name="end_time"></label>
-    <br>
-    <label for ="category">카테고리
-        <select>
-            <option value="">카테고리를 선택해 주세요</option>
-            <option value="book" id = "category">독서</option>
-            <option value="exer" id = "category">운동</option>
-            <option value="study" id = "category">공부</option>
-            <option value="wake" id = "category">기상</option>
-            <option value="hobby" id = "category">취미</option>
-            <option value="meeting" id = "category">회의</option>
-            <option value="shopping" id = "category">쇼핑</option>
-            <option value="cook" id = "category">요리</option>
-            <option value="clean" id = "category">청소</option>
-            <option value="friend" id = "category">친구</option>
-            <option value="family" id = "category">가족</option>
-            <option value="tuor" id = "category">여행</option>
-            <option value="movie" id = "category">영화</option>
-            <option value="rest" id = "category">휴식</option>
-            <option value="opt" id = "category">기타</option>
-            <option value="hospital" id = "category">병원</option>
-            <option value="eat" id = "category">식사</option>
-        </select>
-    </label>
-    <br>
-    <label for ="title">제목 <input type="text" id ="title"></label>
-    <br>
-    <label for ="com">수행여부 <input type="checkbox" id = "com"></label>
-    <br>
-    <label for ="memo">메모 <input type="text" id = "memo"></label>
-    <br>
-    <label for="write_button"><button type ="submit"><a id="write_button">작성</a></button></label>
+        <div class = date_title>
+            <label for = "date_title">
+            <input type="date" name = "task_date" ></label>
+        </div>
+        <br>
+        시작시간<input type="time" name = "start_time" ></label>
+        <br>
+        종료시간<input type="time" name = "end_time" ></label>
+        <br>
+        <label for ="category">카테고리
+        
+            <select name = "category_no">
+                <!-- <option value= 0>카테고리를 선택해 주세요</option> -->
+                <option value= 1 >독서</option>
+                <option value= 2 >운동</option>
+                <option value= 3 >공부</option>
+                <option value= 4 >기상</option>
+                <option value= 5 >취미</option>
+                <option value= 6 >회의</option>
+                <option value= 7 >쇼핑</option>
+                <option value= 8 >요리</option>
+                <option value= 9 >청소</option>
+                <option value= 10 >친구</option>
+                <option value= 11 >가족</option>
+                <option value= 12 >여행</option>
+                <option value= 13 >영화</option>
+                <option value= 14 >휴식</option>
+                <option value= 15 >기타</option>
+                <option value= 16 >병원</option>
+                <option value= 17 >식사</option>
+            </select>
+        </label>
+        <br>
+        <label for ="task_title">제목 <input type="text" name ="task_title"></label>
+        <br>
+        <!-- <label for ="is_com">수행여부 <input type="checkbox" name = "is_com"></label> -->
+        <br>
+        <label for ="task_memo">메모 <input type="text" name = "task_memo"></label>
+        <br>
+        <label for="write_button"><button type ="submit">작성</button></label>
     </form>
 
 

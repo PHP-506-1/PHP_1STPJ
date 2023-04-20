8324df8cc1201bdcc6dd0a57f2264227e2194a06
sjklan4 <126547805+sjklan4@users.noreply.github.com>
2023-04-20 10:56:54 +0900

    0420wirte_tran

--- project/write.php
+++ project/write.php
@@ -105,6 +105,7 @@ function write_info(&$param_arr)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
     <title>작성페이지</title>
 </head>
 <body>

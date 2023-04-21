599b48a52312b23c2641ba041ddf3f587bbc2364
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-21 12:57:33 +0900

    detail photo box

--- project/detail.php
+++ project/detail.php
@@ -1,17 +1,18 @@
 <?php
-    define( "DOC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/" ); // $_SERVER : 슈퍼글로벌 변수 ($_대문자) / 현재 사이트가 위치한 서버상의 위치
-    define( "URL_DB", DOC_ROOT."project/DB/db_conn.php" );
-    include_once( URL_DB );
+define("DOC_ROOT", $_SERVER["DOCUMENT_ROOT"] . "/"); // $_SERVER : 슈퍼글로벌 변수 ($_대문자) / 현재 사이트가 위치한 서버상의 위치
+define("URL_DB", DOC_ROOT . "project/DB/db_conn.php");
+include_once(URL_DB);
 
-    // Request Parameter 획득(GET)
-    $arr_get = $_GET;
+// Request Parameter 획득(GET)
+$arr_get = $_GET;
 
-    // DB에서 게시글 정보 획득
-    $result_info = select_task_info_no( $arr_get["task_no"] );
+// DB에서 게시글 정보 획득
+$result_info = select_task_info_no($arr_get["task_no"]);
 ?>
 
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,11 +21,14 @@
     <link href="./css/main.css" rel="stylesheet" type="text/css">.
     <link href="./css/detail.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
 
     <div class="sidebox">
         <div class="top"></div>
-        <div class="bottom"></div>
+        <div class="bottom">
+            <div class="photo"></div>
+        </div>
     </div>
     <div class="contianer">
         <div class="title top">
@@ -74,4 +78,5 @@
     </div>
 
 </body>
+
 </html>

274d0c1d22278d17ba6c9a918a26f7a3397ccd00
sjklan4 <126547805+sjklan4@users.noreply.github.com>
2023-04-24 12:32:29 +0900

    fatalerror수정

--- project/graph.php
+++ project/graph.php
@@ -4,7 +4,7 @@ define( "URL_DB", DOC_ROOT."project/DB/db_conn.php");
 include_once( URL_DB );
 
 
-$conn = get_db_conn();
+db_conn($conn);
 
 
 
@@ -14,40 +14,21 @@ if ($http_method === "POST"  )
     $start_date = $_POST['start_date'];
     $end_date = $_POST['end_date'];
 }
-// 최근 일주일간의 기상카테고리 시작시간 평균 구하는 쿼리
-$sql1 = "SELECT floor(AVG(TIME_TO_SEC(start_time))) AS avg_start_time
-        FROM Task
-        WHERE category_no = (SELECT category_no FROM Category WHERE category_name = '기상')
-        AND task_date >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) ";
-  
-$result1 = $conn->query($sql1);
-$row1 = $result1->fetchAll();
 
+
+// 최근 일주일간의 기상카테고리 시작시간 평균 구하는 쿼리
+
+$wake_up_result = wake_up_fnc();
 
 // 최근 한달간의 카테고리별 사용 횟수 구하는 쿼리
 
-$sql2 = " SELECT category_name, COUNT(category_name) AS num_count
-        FROM Category c
-        JOIN Task t ON c.category_no = t.category_no
-        WHERE task_date >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
-        AND c.category_name != '기상'
-        GROUP BY c.category_name
-        ORDER BY num_count DESC LIMIT 3 ";
 
-// $limit_num = 5;
-// $stmt->bindParam(':limit_num' , $limit_num, PDO::PARAM_INT);
-// $stmt->execute();
-$result2 = $conn->query($sql2);
+$count_month_fnc = month_cnt();
 
 
 
 
-$sql3 = "SELECT 
-            (SELECT COUNT(*) FROM task WHERE is_com = '1') /
-            (SELECT COUNT(*) FROM task) AS completion_ratio";
-$result3 = $conn->query($sql3);
-$evalu = $result3->fetchall();
-
+$avg_fnc = value_avg_fnc();
 // var_dump($result3);
 // $sql3 = " SELECT 
 // 	(SELECT COUNT(*) FROM task where is_com = '1')/
@@ -160,11 +141,11 @@ $evalu = $result3->fetchall();
                     
                         <tr>
                             <td>최근 한달 평균 기상시간: </td>
-                            <td> <?php foreach ($row1 as $avg){ ?> <?php echo gmdate("H시 i분 s초", $avg["avg_start_time"]);?> <?php } ?></td>
+                            <td> <?php foreach ($wake_up_result as $avg){ ?> <?php echo gmdate("H시 i분 s초", $avg["avg_start_time"]);?> <?php } ?></td>
                         </tr>
 
                         <tr>
-                            <td>최근 한달 활동 TOP3<span>(기상제외)</span> <?php $i = 1; foreach ($result2 as $row2) { ?>
+                            <td>최근 한달 활동 TOP3<span>(기상제외)</span> <?php $i = 1; foreach ($count_month_fnc as $row2) { ?>
                             </td>
                         </tr>
                         <tr>
@@ -181,7 +162,7 @@ $evalu = $result3->fetchall();
             <div>
                 <tr>
                         <td>
-                            수행율: <?php foreach ($evalu as $completion_ratio) {  echo $completion_ratio['completion_ratio']* 100; }?> % 
+                            수행율: <?php foreach ($avg_fnc as $completion_ratio) {  echo $completion_ratio['completion_ratio']* 100; }?> % 
                         </td>
                         <td>
                             <div class = "sticy">
